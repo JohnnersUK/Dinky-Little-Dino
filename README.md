2b498c45b407d277ef9722cf48157416dbb0110a
James Huxtable <HuxyUK@users.noreply.github.com>
2017-11-04 13:40:45 +0000

    Endless Runner (Game #3) - added git ignore file - latest version of ASGE provided - full API documentation can be found in Docs/html/index.html - basic boilerplate code added

---
+++ README.md
@@ -0,0 +1,13 @@
+# Endless Runner 
+An endless runner style game based on the dissection of Temple Run in the 1st Year.  
+
+"Endless running" or "infinite running" games are platform games in which the player character is continuously moving forward through a usually procedurally generated, theoretically endless game world. Game controls are limited to making the character jump, attack, or perform special actions.
+
+## Build Instructions
+Open the solution in Visual Studio 2017. The game uses the ASGE framework. The projects are prelinked and can be launched either with debugging or without. Resources will be copied to the builds directory, but make sure the working directory in the debugger is set to $(OutDir).
+
+## How to Play
+Player one controls with... 
+
+
+
---
+++ Source/Game.cpp
@@ -0,0 +1,62 @@
+#include <Engine\GameTime.h>
+#include <Engine\InputEvents.h>
+
+#include "Game.h"
+#include "GameFont.h"
+
+EndlessGame::~EndlessGame()
+{
+	this->inputs->unregisterCallback(key_handler_id);
+
+	for (auto& font : GameFont::fonts)
+	{
+		delete font;
+		font = nullptr;
+	}
+}
+
+bool EndlessGame::init()
+{
+	game_width = 1280;
+	game_height = 720;
+
+	if (!initAPI(ASGE::Renderer::WindowMode::WINDOWED))
+	{
+		return false;
+	}
+
+	renderer->setWindowTitle("Endless Runner");
+	renderer->setClearColour(ASGE::COLOURS::BLACK);
+	renderer->setSpriteMode(ASGE::SpriteSortMode::IMMEDIATE);
+	toggleFPS();
+
+	this->inputs->use_threads = false;
+	this->inputs->addCallbackFnc(
+		ASGE::EventType::E_KEY, &EndlessGame::keyHandler, this);
+
+	return true;
+}
+
+void EndlessGame::update(const ASGE::GameTime& us)
+{
+
+}
+
+void EndlessGame::render(const ASGE::GameTime& us)
+{
+
+}
+
+void EndlessGame::keyHandler(const ASGE::SharedEventData data)
+{
+	ASGE::KeyEvent* key_event = static_cast<ASGE::KeyEvent*>(data.get());
+	
+	auto key = key_event->key;
+	auto action = key_event->action;
+
+	if (key == ASGE::KEYS::KEY_ESCAPE)
+	{
+		signalExit();
+	}
+}
+
